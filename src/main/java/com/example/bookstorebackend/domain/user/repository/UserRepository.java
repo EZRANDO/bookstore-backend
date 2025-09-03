17ff1db25bebf3a6df1b321f9d26f9cd25cb8f89
EZRANDO <gpdnjs2578@naver.com>
2025-09-03 09:49:15 +0900

    [feat]user 쿼리 메서드 추가

--- src/main/java/com/example/bookstorebackend/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/bookstorebackend/domain/user/repository/UserRepository.java
@@ -15,4 +15,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByEmail(String email);
 
+    Optional<Object> findByIdAndDeletedAtIsNull(Long userId);
+
 }
