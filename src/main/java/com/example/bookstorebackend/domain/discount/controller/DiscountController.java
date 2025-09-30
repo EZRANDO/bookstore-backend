86c2e8e82c2ad7ba044998b4c538421cd59f29f8
EZRANDO <gpdnjs2578@naver.com>
2025-09-30 23:59:59 +0900

    [feat]비즈니스 로직, controller클래스 추가

---
+++ src/main/java/com/example/bookstorebackend/domain/discount/controller/DiscountController.java
@@ -0,0 +1,11 @@
+package com.example.bookstorebackend.domain.discount.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/discount")
+public class DiscountController {
+
+
+}
